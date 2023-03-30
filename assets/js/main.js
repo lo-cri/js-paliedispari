379e059b39784ca568738c0e6740f88188150dd8
lo-cri <criloffredi@gmail.com>
2023-03-30 14:10:59 +0200

    esercizio palindromo fatto

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+//ESERCIZIO PALINDROMA
+
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// const parola = prompt("Inserisci una parola:");
+
+
+// function isPalindroma(parola) {
+//   parola = parola.toLowerCase().replace(/\s/g, "");
+//   const caratteri = parola.split("")
+//   const caratteriInvertiti = caratteri.reverse();
+//   const parolaInvertita = caratteriInvertiti.join("");
+
+//   if (parola === parolaInvertita) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// if (isPalindroma(parola)) {
+//   console.log("La parola inserita è palindroma.");
+// } else {
+//   console.log("La parola inserita non è palindroma.");
+// }
+
+
+
+
+
