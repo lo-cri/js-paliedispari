9feaf1aeebd1ebdf73f544a6d7c8104a8b9cee79
lo-cri <criloffredi@gmail.com>
2023-04-02 14:54:57 +0200

    pari e dispari fatto

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,22 @@
 //   console.log("La parola inserita non è palindroma.");
 // }
 
+// ESERCIZIO PARI E DISPARI
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
 
+const scelta = prompt("Scegli pari o dispari:");
+const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5:"));
 
+const numeroComputer = Math.floor(Math.random() * 4) + 1;
 
+const somma = numeroUtente + numeroComputer;
 
+if ((scelta === "pari" && somma % 2 === 0) ) {
+    console.log("Hai vinto");
+} else {
+    console.log("Ha vinto il computer");
+}
